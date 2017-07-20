b5fbef3694828ea3ebd6b7e75b032b6cebbb8748
Tommy Kvant <tommy.kvant@gmail.com>
2017-07-20 21:41:58 +0200

    Changed the project to a backend service instead

---
+++ controllers/board.js
@@ -0,0 +1,13 @@
+'use strict';
+
+exports.getBoard = (req, res) => {
+    const width = req.params.width;
+    const height = req.params.height;
+
+    // create board
+
+    // while board is not full:
+    //     get word max length of space left on board
+    //     place word on board
+    //     shift arbitrary amount up/down on board
+}
